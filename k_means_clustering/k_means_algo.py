2ebdef3145a5d71d6542b06ab8840610d85eb8d2
Arun Joshi <jarun6069@gmail.com>
2024-09-10 08:59:51 +0545

    3d plots added

--- k_means_clustering/k_means_algo.py
+++ k_means_clustering/k_means_algo.py
@@ -64,9 +64,13 @@ def plot(type = 'hist'):
 plot()
 plot(type = 'scatter')
 
-def elbow_plot():
-    input_features = ['Annual Income', 'Spending Score']
-    X = df[input_features]
+def elbow_plot(type = '2d'):
+    if type == '2d':
+        input_features = ['Annual Income', 'Spending Score']
+        X = df[input_features]
+    if type == '3d': 
+        input_features = ['Age', 'Annual Income', 'Spending Score']
+        X = df[input_features]
     ''' This function plots the elbow method to show the most optimal
     k value for the k-means algorithm'''
     model = KMeans(random_state = 42)
@@ -79,10 +83,15 @@ elbow_plot()
 '''The elbow plot at k = 5, seems to be the best hyperparameter for K as the 
 distortion score seems to be the lowest.'''
 
-def model():
-    input_features = ['Annual Income', 'Spending Score']
-    X = df[input_features]
-    model = KMeans(n_clusters=5)
+def model(type = '2d'):
+    if type == '2d':
+        input_features = ['Annual Income', 'Spending Score']
+        X = df[input_features]
+        model = KMeans(n_clusters=5)
+    if type == '3d':
+        input_features = ['Age' ,'Annual Income', 'Spending Score']
+        X = df[input_features]
+        model = KMeans(n_clusters=6)
     model.fit(X)
     labels = model.labels_
     centroids = model.cluster_centers_
@@ -92,7 +101,7 @@ model1, labels1, centroids1 = model()
 print(labels1)
 print(centroids1)
 
-def plotting_knn():
+def plotting_k_means():
     #Plotting the data
     plt.scatter(df['Annual Income'], df['Spending Score'], 
                 c = labels1, cmap = 'viridis', marker = 'o')
@@ -105,7 +114,7 @@ def plotting_knn():
     plt.show()
 
 
-plotting_knn()
+plotting_k_means()
 
 
 def plotting_decision_boundary():
@@ -143,6 +152,78 @@ plotting_decision_boundary()
 ''' Below is the further modeling of the data by using 3 input features and 
 plotting a 3d model plot.'''
 
+def three_dim_plot():
+    fig = plt.figure(figsize=(12,12))
+    ax = fig.add_subplot(projection = '3d')
+    X = df['Age']
+    Y = df['Annual Income']
+    Z = df['Spending Score']
+    ax.scatter(X,Y,Z, marker = 'o')
+    ax.set_xlabel('Age')
+    ax.set_ylabel('Annual Income')
+    ax.set_zlabel('Spending Score')
+    plt.show()
+    
+three_dim_plot()
+elbow_plot(type = '3d')
+''' According to elbow plot, when k =6 the distortion score is the lowest. Thus, 
+making k = 6 the best hypermater setting for the model.'''
+
+model2, labels2, centroids2 = model(type = '3d')
+print(labels2)
+print(centroids2)
+
+def model_plot_3d():
+    fig = plt.figure(figsize=(12,12))
+    ax = fig.add_subplot(projection = '3d')
+    X = df['Age']
+    Y = df['Annual Income']
+    Z = df['Spending Score']
+    #Plotting the data
+    ax.scatter(X, Y, Z, 
+                c = labels2, cmap = 'viridis', marker = 'o')
+    #Plotting the centroids
+    plt.scatter(centroids2[:, 0], centroids2[:, 1],centroids2[:, 2], 
+                color = 'red', marker = 'x', label = 'Centroids')
+    # Creating a meshgrid to plot decision boundaries
+    x_min, x_max = X.min() - 1, X.max() + 1
+    y_min, y_max = Y.min() - 1, Y.max() + 1
+    z_min, z_max = Z.min() - 1, Z.max() + 1
+
+    xx, yy, zz = np.meshgrid(np.linspace(x_min, x_max, 50),
+                             np.linspace(y_min, y_max, 50),
+                             np.linspace(z_min, z_max, 50))
+    
+    # Stack the grid to pass it to the model
+    grid = np.c_[xx.ravel(), yy.ravel(), zz.ravel()]
+    
+    # Predicting the cluster for each point in the grid
+    labels_grid = model2.predict(grid)
+    
+    # Reshaping the result to match the shape of the grid
+    labels_grid = labels_grid.reshape(xx.shape)
+    
+    # Plotting decision boundary surfaces
+    ax.plot_surface(xx[:, :, 0], yy[:, :, 0], zz[:, :, 0], 
+                    facecolors=plt.cm.viridis(labels_grid[:, :, 0] / labels_grid.max()), 
+                    alpha=0.3, rstride=100, cstride=100)
+    ax.set_xlabel('Age')
+    ax.set_xlabel('Annual Income')
+    ax.set_xlabel('Spending Score')
+    plt.title('K Means Clustering')
+    plt.show()
+    
+    
+model_plot_3d()
+
+from mpl_toolkits import mplot3d
+def test_plot():
+    ax = plt.axes(projection = '3d')
+    ax.scatter(3,5,7)
+    plt.show()
+    
+test_plot()
+    
 
     
 
